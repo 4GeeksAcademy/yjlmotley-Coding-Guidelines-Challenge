bf8e51ef9747299d79f84f7604e9e25b8579b0c1
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-01-13 01:23:57 +0000

    created function to less complicate the code

--- src/app.js
+++ src/app.js
@@ -11,27 +11,30 @@ window.onload = () => {
   document.querySelector("#button").addEventListener("click", () => {
     document.querySelector("#excuse").innerHTML = generateExcuse();
   });
-  console.log("Hello Rigo from the console!");
 };
 
 let generateExcuse = () => {
   let who = ["A dog", "A stranger", "A cat", "The squirrel", "A crow"];
   let action = ["stole my", "grabbed my", "ripped my", "pooped on my"];
   let what = ["homework", "lunch money", "backpack", "hat"];
-  let when = ["this morning!", "on the street!", "in the park!", "right outside the door!"];
+  let when = [
+    "this morning!",
+    "on the street!",
+    "in the park!",
+    "right outside the door!"
+  ];
 
-  let whoIndx = Math.floor(Math.random() * who.length);
-  let actIndx = Math.floor(Math.random() * action.length);
-  let whatIndx = Math.floor(Math.random() * what.length);
-  let whenIndx = Math.floor(Math.random() * when.length);
+  function random(indx) {
+    return Math.floor(Math.random() * indx.length);
+  }
 
   return (
-    who[whoIndx] +
+    who[random(who)] +
     " " +
-    action[actIndx] +
+    action[random(action)] +
     " " +
-    what[whatIndx] +
+    what[random(what)] +
     " " +
-    when[whenIndx]
+    when[random(when)]
   );
 };
